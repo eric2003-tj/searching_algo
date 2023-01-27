2ada4de25a5664535b46a141cec9176696317b54
Eric <eric.tj1026@gmail.com>
2023-01-27 21:01:53 +0800

    iterative

--- binary_search.c
+++ binary_search.c
@@ -1,20 +1,23 @@
 #include <stdio.h>
-int binary_search(int *arr,int target,int left,int right){
-   int mid = (left+right)/2;
-   if(arr[mid] == target){
-      return mid;
+int binary_search(int *arr,int target,int size){
+   int left = 0;
+   int right = size-1;
+   while(left<=right){
+      int mid = (left+right)/2;
+      if(arr[mid]==target){
+        return mid;
+      }
+      if(arr[mid] > target){
+        right = mid-1;
+      }
+      if(arr[mid] < target){
+        left = mid+1;
+      }
    }
-   if(left > right){
-      return -1;
-   }
-   if(arr[mid]>target){
-      return binary_search(arr,target,left,mid-1);
-   }else{
-      return binary_search(arr,target,mid+1,right);
-   }
+   return -1;
 }
 int main(){
    int arr[] = {2,3,4,5,7,9};
-   printf("%d\n",binary_search(arr,1,0,5)); //print -1
-   printf("%d",binary_search(arr,5,0,5)); //print 3
+   printf("%d\n",binary_search(arr,1,6)); //print -1
+   printf("%d",binary_search(arr,5,6)); //print 3
 }
